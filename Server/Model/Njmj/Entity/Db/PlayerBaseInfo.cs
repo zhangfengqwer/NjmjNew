b6fb288c31a88404ab510cdf9f032fdb4fa34042
zhangmeiyu <511559748@qq.com>
2018-05-07 09:38:34 +0800

    PlayerBaseInfo

--- Server/Model/Njmj/Entity/Db/PlayerBaseInfo.cs
+++ Server/Model/Njmj/Entity/Db/PlayerBaseInfo.cs
@@ -3,6 +3,7 @@ namespace ETModel
 {
     public class PlayerBaseInfo : EntityDB
     {
+        public long uid { get; set; }
         public string Name { get; set; }
         public int GoldNum { get; set; }
         public int WingNum { get; set; }
