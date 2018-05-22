eb8d159057d22e09523736a82a3c303b027bc639
hpzsr <582254172@qq.com>
2018-05-22 12:48:26 +0800

    11

--- Server/Hotfix/Njmj/Handler/Gate/C2G_UseHuaFeiHandle.cs
+++ Server/Hotfix/Njmj/Handler/Gate/C2G_UseHuaFeiHandle.cs
@@ -14,36 +14,11 @@ namespace ETHotfix
             try
             {
                 DBProxyComponent proxyComponent = Game.Scene.GetComponent<DBProxyComponent>();
-                if (message.HuaFei == 1)
-                {
-                    List<UseHuaFei> useHuaFeis = await proxyComponent.QueryJson<UseHuaFei>($"{{Uid:{message.Uid},HuaFei:{message.HuaFei}}}");
-                    if (useHuaFeis.Count > 0)
-                    {
-                        // 不合法的金额
-                        response.Error = ErrorCode.TodayHasSign;
-                        response.Message = "您的1元兑换机会已用完";
-                        reply(response);
-
-                        return;
-                    }
-                    else
-                    {
-                        // 充值1元话费
-                        UseHuaFei useHuaFei = ComponentFactory.CreateWithId<UseHuaFei>(IdGenerater.GenerateId());
-                        useHuaFei.Uid = message.Uid;
-                        useHuaFei.HuaFei = message.HuaFei;
-                        useHuaFei.Phone = message.Phone;
-                        await proxyComponent.Save(useHuaFei);
-
-                        reply(response);
-                    }
-                }
-                else if ((message.HuaFei == 5) || (message.HuaFei == 10) || (message.HuaFei == 20))
+                if (message.HuaFei == 5)
                 {
                     List<UseHuaFei> useHuaFeis = await proxyComponent.QueryJson<UseHuaFei>($"{{CreateTime:/^{DateTime.Now.GetCurrentDay()}/,Uid:{message.Uid},HuaFei:{message.HuaFei}}}");
                     if (useHuaFeis.Count > 0)
                     {
-                        // 不合法的金额
                         response.Error = ErrorCode.TodayHasSign;
                         response.Message = "您今天的兑换机会已用完";
                         reply(response);
