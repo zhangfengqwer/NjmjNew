6b33f253622b95b43ca095df211d57c49ebf906d
zhangmeiyu <511559748@qq.com>
2018-05-30 14:02:50 +0800

    活动

--- Unity/Hotfix/UI/UIActivity/Component/UINoticeItemComponent.cs
+++ Unity/Hotfix/UI/UIActivity/Component/UINoticeItemComponent.cs
@@ -19,6 +19,7 @@ namespace ETHotfix
     public class UINoticeItemComponent : Component
     {
         private Text Content;
+        private Text Title;
         private GameObject Line;
         private GameObject Flag;
         private Button NoticeBtn;
@@ -31,7 +32,7 @@ namespace ETHotfix
             Line = rc.Get<GameObject>("Line");
             Flag = rc.Get<GameObject>("Flag");
             NoticeBtn = rc.Get<GameObject>("NoticeBtn").GetComponent<Button>();
-
+            Title = rc.Get<GameObject>("Title").GetComponent<Text>();
             NoticeBtn.onClick.Add(() =>
             {
                 string key = $"{PlayerInfoComponent.Instance.uid}{info.id}";
@@ -44,6 +45,7 @@ namespace ETHotfix
         public void SetText(NoticeInfo info)
         {
             this.info = info;
+            Title.text = info.title;
             Content.text = info.content;
             string key = $"{PlayerInfoComponent.Instance.uid}{info.id}";
             int state = PlayerPrefs.GetInt(key);
