9431f5b969caceef7552877b734e3cc2f1a3a2b7
zhangmeiyu <511559748@qq.com>
2018-06-12 14:42:00 +0800

    去掉背包数据

--- Server/Hotfix/Njmj/Handler/Gate/C2G_LoginGateHandler.cs
+++ Server/Hotfix/Njmj/Handler/Gate/C2G_LoginGateHandler.cs
@@ -65,60 +65,6 @@ namespace ETHotfix
                     shopInfoList.Add(info);
                 }
 
-                #region AddItemInfo
-                List<UserBag> itemInfoList = await proxyComponent.QueryJson<UserBag>($"{{UId:{userId}}}");
-                if (itemInfoList.Count <= 0)
-                {
-                    {
-                        UserBag item = new UserBag();
-                        item.BagId = 104;
-                        item.Count = 10;
-                        item.UId = userId;
-                        DBHelper.AddItemToDB(item);
-                    }
-
-                    {
-                        UserBag item = new UserBag();
-                        item.BagId = 105;
-                        item.Count = 10;
-                        item.UId = userId;
-                        DBHelper.AddItemToDB(item);
-                    }
-
-                    {
-                        UserBag item = new UserBag();
-                        item.BagId = 107;
-                        item.Count = 10;
-                        item.UId = userId;
-                        DBHelper.AddItemToDB(item);
-                    }
-
-                    {
-                        UserBag item = new UserBag();
-                        item.BagId = 108;
-                        item.Count = 10;
-                        item.UId = userId;
-                        DBHelper.AddItemToDB(item);
-                    }
-
-                    {
-                        UserBag item = new UserBag();
-                        item.BagId = 109;
-                        item.Count = 10;
-                        item.UId = userId;
-                        DBHelper.AddItemToDB(item);
-                    }
-
-                    {
-                        UserBag item = new UserBag();
-                        item.BagId = 111;
-                        item.Count = 10;
-                        item.UId = userId;
-                        DBHelper.AddItemToDB(item);
-                    }
-                }
-                #endregion
-
                 //添加消息转发组件
                 await session.AddComponent<ActorComponent, string>(ActorType.GateSession).AddLocation();
 
