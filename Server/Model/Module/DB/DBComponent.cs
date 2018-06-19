55f0cc6fba3b867929ed3938abc459a50630e1da
hpzsr <582254172@qq.com>
2018-06-19 18:38:12 +0800

    11

--- Server/Model/Module/DB/DBComponent.cs
+++ Server/Model/Module/DB/DBComponent.cs
@@ -37,6 +37,31 @@ namespace ETModel
             return this.database.GetCollection<PlayerBaseInfo>(name);
         }
 
+        public IMongoCollection<AccountInfo> GetAccountInfoCollection(string name)
+        {
+            return this.database.GetCollection<AccountInfo>(name);
+        }
+
+        public IMongoCollection<Log_Login> GetLogLoginCollection(string name)
+        {
+            return this.database.GetCollection<Log_Login>(name);
+        }
+
+        public IMongoCollection<Log_OldUserBind> GetOldUserBindnCollection(string name)
+        {
+            return this.database.GetCollection<Log_OldUserBind>(name);
+        }
+
+        public IMongoCollection<Log_Recharge> GetLogRechargeCollection(string name)
+        {
+            return this.database.GetCollection<Log_Recharge>(name);
+        }
+
+        public IMongoCollection<Log_Game> GetLogGameCollection(string name)
+        {
+            return this.database.GetCollection<Log_Game>(name);
+        }
+
         public IMongoCollection<EmailInfo> GetEmailInfoCollection(string name)
         {
             return this.database.GetCollection<EmailInfo>(name);
