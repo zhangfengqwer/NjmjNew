b731925971a97894e4ed885d81a6b18564cb78c1
hpzsr <582254172@qq.com>
2018-06-25 14:00:01 +0800

    11

--- Unity/Hotfix/UI/UICommonPanel/Component/UICommonPanelComponent.cs
+++ Unity/Hotfix/UI/UICommonPanel/Component/UICommonPanelComponent.cs
@@ -37,12 +37,10 @@ namespace ETHotfix
 
         public static UICommonPanelComponent showCommonPanel(string title,string content)
         {
-            if (Game.Scene.GetComponent<UIComponent>().Get(UIType.UICommonPanel) != null)
+            if (Game.Scene.GetComponent<UIComponent>().Get(UIType.UICommonPanel) == null)
             {
-                Game.Scene.GetComponent<UIComponent>().Remove(UIType.UICommonPanel);
-            }
-
-            Game.Scene.GetComponent<UIComponent>().Create(UIType.UICommonPanel);
+                Game.Scene.GetComponent<UIComponent>().Create(UIType.UICommonPanel);
+            }            
 
             if (Game.Scene.GetComponent<UIComponent>().Get(UIType.UICommonPanel) != null)
             {
