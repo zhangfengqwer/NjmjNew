8c44fa34a6869cdc555b51ffe779072218cefe19
zf <984992820@qq.com>
2018-06-29 15:18:21 +0800

    1

---
+++ Server/Hotfix/Njmj/Handler/Gate/C2G_ChangeAccountHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using ETModel;
+using MongoDB.Driver;
+
+namespace ETHotfix
+{
+	[MessageHandler(AppType.Gate)]
+	public class C2G_ChangeAccountHandler : AMRpcHandler<C2R_ChangeAccount, R2C_ChangeAccount>
+	{
+	    protected override async void Run(Session session, C2R_ChangeAccount message, Action<R2C_ChangeAccount> reply)
+	    {
+	        Log.Info(JsonHelper.ToJson(message));
+            R2C_ChangeAccount response = new R2C_ChangeAccount();
+	        try
+	        {
+                Game.Scene.GetComponent<UserComponent>().Remove(message.Uid);
+	            reply(response);
+	        }
+	        catch (Exception e)
+	        {
+	            ReplyError(response, e, reply);
+	        }
+	    }
+	}
+}
