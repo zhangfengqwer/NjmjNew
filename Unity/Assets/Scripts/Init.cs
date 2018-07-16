eceb661cf73bb9468c6c931a565930bee41bbb0e
zf <984992820@qq.com>
2018-07-16 13:52:06 +0800

    1

--- Unity/Assets/Scripts/Init.cs
+++ Unity/Assets/Scripts/Init.cs
@@ -40,9 +40,9 @@ namespace ETModel
 
 	            //新增Component
 	            Game.Scene.AddComponent<SoundComponent>();
-
-	            // 获取网络配置表
+	                // 获取网络配置表
 	            await NetConfig.getInstance().Req("http://fwdown.hy51v.com/njmj/online/files/netconfig.json");
+
 	            // 下载ab包
 	            await BundleHelper.DownloadBundle();
 	         
